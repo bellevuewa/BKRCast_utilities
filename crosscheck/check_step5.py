97698eb8d73aad3a100408709397752b6299988e
oangah <oangah@bellevuewa.gov>
2025-06-10 09:40:02 -0700

    [update] updated checker for step 5

--- crosscheck/check_step5.py
+++ crosscheck/check_step5.py
@@ -42,6 +42,27 @@ for city in city_to_check:
             "Synchronizng synthetic population file to parcel file (step 5) is not correct!"
     print('-----')
 
-    parcel_df_after
+# summarize parcel urbansim file to TAZ, subarea and city level.
+# the scripts below is revised from BKRCast_Tools-Python3/LandUse/parcel_file_summary.py
+output_field = ['EMPEDU_P', 'EMPFOO_P', 'EMPGOV_P', 'EMPIND_P', 'EMPMED_P', 'EMPOFC_P', 'EMPOTH_P', 'EMPRET_P', 'EMPSVC_P', 'EMPTOT_P', 'STUGRD_P', 'STUHGH_P', 'STUUNI_P', 'HH_P']
+
+taz_subarea = pd.read_csv(os.path.join(working_folder_lu, subarea_file), sep = ',')
+
+# parcel_df_after = parcel_df_after.merge(taz_subarea, how = 'left',  left_on = 'TAZ_P', right_on = 'BKRCastTAZ')
+parcel_df_after = parcel_df_after.merge(parcel_taz_lookup, how = 'left',  left_on = 'PARCELID', right_on = 'PSRC_ID')
+
+summary_by_jurisdiction = parcel_df_after.groupby('Jurisdiction')[output_field].sum()
+print("Exporting... \"summary_by_jurisdiction.csv\"")
+summary_by_jurisdiction.to_csv(os.path.join(working_folder_lu, "summary_by_jurisdiction.csv"))
+summary_by_taz = parcel_df_after.groupby('TAZ_P')[output_field].sum()
+print("Exporting... \"summary_by_TAZ.csv\"")
+summary_by_taz.to_csv(os.path.join(working_folder_lu, "summary_by_TAZ.csv")) 
+print("Exporting... \"summary_by_subarea.csv\"")
+parcel_df_after = parcel_df_after.merge(taz_subarea[['BKRCastTAZ', 'Subarea', 'SubareaName']], how = 'left',  left_on = 'TAZ_P', right_on = 'BKRCastTAZ')
+summary_by_subarea = parcel_df_after[parcel_df_after['Subarea'] > 0].groupby('Subarea')[output_field].sum()
+taz_subarea = parcel_df_after[['Subarea', 'SubareaName']].drop_duplicates()
+taz_subarea.set_index('Subarea', inplace = True)
+summary_by_subarea = summary_by_subarea.join(taz_subarea['SubareaName'])
+summary_by_subarea.to_csv(os.path.join(working_folder_lu, "summary_by_subarea.csv"))
 
 print('Done')
